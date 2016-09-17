b1adc342dea4dae11cee94b2e0827e753a5bc428
2xC <ckchango@gmail.com>
2016-09-17 15:36:03 -0500

    _

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOfConcept.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOfConcept.java
@@ -57,7 +57,7 @@ import java.util.Date;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@TeleOp(name="TestOfConcept", group="Cool Stuff")  // @Autonomous(...) is the other common choice
 @Disabled
 public class TestOfConcept extends OpMode {
 
