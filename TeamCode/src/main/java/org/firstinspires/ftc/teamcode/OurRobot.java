d6130ed9c3ba1eb0a43c1a95c52a9234fd1fa2bf
loris <loris.battista@gmail.com>
2016-11-12 14:21:24 -0600

    Added throwerMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurRobot.java
@@ -27,7 +27,7 @@ public class OurRobot
     /* Public OpMode members. */
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
-    public DcMotor  armMotor    = null;
+    public DcMotor  throwerMotor    = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
 
@@ -51,21 +51,21 @@ public class OurRobot
 
         // Define and Initialize Motors
         leftDrive   = hwMap.dcMotor.get("leftDrive");
-        rightDrive  = hwMap.dcMotor.get("rightDrive");
-        armMotor    = hwMap.dcMotor.get("armMotor");
+        rightDrive    = hwMap.d  = hwMap.dcMotor.get("rightDrive");
+        throwerMotor.get("armMotor");
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        armMotor.setPower(0);
+        throwerMotor.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        throwerMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
         leftClaw = hwMap.servo.get("leftHand");
